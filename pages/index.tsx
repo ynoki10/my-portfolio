64fac11015513c8e564732a799d09b2e6e551b0a
ynoki10 <sakanafront@gmail.com>
2024-02-03 20:26:56 +0900

    shadcn/ui 導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <h1>Yoshinoki&apos;s PortFolio</h1>
+      <div>
+        <Button variant="outline" size="lg">
+          Click me
+        </Button>
+      </div>
     </main>
   );
 }
