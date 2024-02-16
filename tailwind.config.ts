07d2cda61e39f4526d58e23f245ba545cd8ac52c
ynoki10 <sakanafront@gmail.com>
2024-02-16 21:32:34 +0900

    proseの最大幅を100%に変更

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,6 +67,13 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+          },
+        },
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
