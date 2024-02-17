e08a3eaf9fef1e3585f052ca70be0287fe9e952e
ynoki10 <sakanafront@gmail.com>
2024-02-17 18:45:23 +0900

    プロフィールページ上部エリアをコンポーネント分割

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
 
-import Image from '@/components/ui/Image';
+import ProfileHeader from '@/components/site-parts/ProfileHeader';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 
 import { genMetadata } from '@/app/metadata';
@@ -44,44 +44,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
   return (
     <article className="space-y-10">
       <h1 className="text-3xl font-bold tracking-tighter">{page.title}</h1>
-      {page.slug === 'profile' && (
-        <div className="flex flex-col items-center gap-y-4">
-          <Image
-            alt="プロフィール画像：はっきりとした線で描かれたポップなイラスト。男性が前方を見上げて口角を上げている。空をイメージした青い背景。"
-            className="size-48 rounded-full"
-            height={192}
-            src="/images/icon.webp"
-            width={192}
-          />
-          <p className="text-2xl font-bold">Yoshinoki</p>
-          <ul className="flex space-x-4">
-            {[
-              {
-                href: 'https://github.com/ynoki10',
-                text: 'GitHub',
-              },
-              {
-                href: 'https://twitter.com/4noki10',
-                text: 'Twitter(X)',
-              },
-              {
-                href: 'https://www.yoshinoki.dev/',
-                text: 'Blog',
-              },
-              {
-                href: 'https://zenn.dev/yoshinoki',
-                text: 'Zenn',
-              },
-            ].map(({ href, text }) => (
-              <li key={text}>
-                <a className="text-indigo-800 underline" href={href} target="_blank">
-                  {text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {page.slug === 'profile' && <ProfileHeader />}
       <div className="prose prose-slate">
         <div dangerouslySetInnerHTML={{ __html: page.body }}></div>
         {page.accordions &&
---
+++ components/site-parts/ProfileHeader/index.tsx
@@ -0,0 +1,44 @@
+import Image from '@/components/ui/Image';
+
+const ProfileHeader = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <Image
+        alt="プロフィール画像：はっきりとした線で描かれたポップなイラスト。男性が前方を見上げて口角を上げている。空をイメージした青い背景。"
+        className="size-48 rounded-full"
+        height={192}
+        src="/images/icon.webp"
+        width={192}
+      />
+      <p className="text-2xl font-bold">Yoshinoki</p>
+      <ul className="flex space-x-4">
+        {[
+          {
+            href: 'https://github.com/ynoki10',
+            text: 'GitHub',
+          },
+          {
+            href: 'https://twitter.com/4noki10',
+            text: 'Twitter(X)',
+          },
+          {
+            href: 'https://www.yoshinoki.dev/',
+            text: 'Blog',
+          },
+          {
+            href: 'https://zenn.dev/yoshinoki',
+            text: 'Zenn',
+          },
+        ].map(({ href, text }) => (
+          <li key={text}>
+            <a className="text-indigo-800 underline" href={href} target="_blank">
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProfileHeader;
