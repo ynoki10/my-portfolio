ad54a7efc837da6ebd2feb75a03139930678f8ef
ynoki10 <sakanafront@gmail.com>
2024-02-18 15:31:44 +0900

    ハンバーガーメニュー スタイル調整

--- components/site-parts/Header/HeaderHamburger.tsx
+++ components/site-parts/Header/HeaderHamburger.tsx
@@ -15,13 +15,12 @@ type Props = {
 
 const HeaderHamburger = ({ pages }: Props) => {
   const pathname = usePathname();
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   // ページ遷移を検知
   useEffect(() => {
     closeDialog();
   }, [pathname]);
-
-  const dialogRef = useRef<HTMLDialogElement>(null);
 
   const openDialog = () => {
     dialogRef.current?.showModal();
@@ -30,26 +29,39 @@ const HeaderHamburger = ({ pages }: Props) => {
     dialogRef.current?.close();
   };
 
+  const handleDialogClick = (e: React.MouseEvent<HTMLDialogElement>) => {
+    if (e.target === e.currentTarget) {
+      closeDialog();
+    }
+  };
+
   return (
     <div className="lg:hidden">
-      <dialog aria-label="メニュー" ref={dialogRef} className="relative p-10">
-        <button
-          type="button"
-          aria-label="メニューを閉じる"
-          className="absolute right-1  top-0 p-2"
-          onClick={closeDialog}
-        >
-          <span aria-hidden="true" className="text-3xl">
-            ×
-          </span>
-        </button>
-        <nav className="flex flex-col gap-4">
-          {pages.map((page) => (
-            <Link key={page.id} href={`/${page.slug}/`}>
-              {page.title}
-            </Link>
-          ))}
-        </nav>
+      <dialog
+        aria-label="メニュー"
+        ref={dialogRef}
+        className="relative  backdrop:bg-slate-700 backdrop:opacity-60 lg:hidden"
+        onClick={handleDialogClick}
+      >
+        <div className="p-16">
+          <button
+            type="button"
+            aria-label="メニューを閉じる"
+            className="absolute right-3  top-0 p-2"
+            onClick={closeDialog}
+          >
+            <span aria-hidden="true" className="text-3xl">
+              ×
+            </span>
+          </button>
+          <nav className="flex flex-col gap-6">
+            {pages.map((page) => (
+              <Link key={page.id} href={`/${page.slug}/`} className="py-1 hover:underline focus-visible:underline">
+                {page.title}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </dialog>
       <button type="button" aria-label="メニューを開く" className="p-2" onClick={openDialog}>
         <span aria-hidden="true" className="text-3xl">
